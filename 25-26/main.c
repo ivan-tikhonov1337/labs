c50a6d3ae54bdd99e906f51a56cc25642a763f53
ivan-tikhonov1337 <127095794+ivan-tikhonov1337@users.noreply.github.com>
2023-04-26 13:43:54 +0300

    Update main.c

--- 25-26/main.c
+++ 25-26/main.c
@@ -5,21 +5,21 @@
 
 #define INF INT_MAX
 
-data procedure(udt *d) {                      // Находит максимальный элемент в деке и удаляет его, перемещая все остальные элементы влево или вправо,
-    data t, a;                                // чтобы сохранить порядок элементов. Затем он возвращает максимальный элемент.
+int procedure(udt *d) {                      // Находит максимальный элемент в деке и удаляет его, перемещая все остальные элементы влево или вправо,
+    int t, a;                                // чтобы сохранить порядок элементов. Затем он возвращает максимальный элемент.
     int max = -INF - 1;
     int size = d->size;
     for (int i = 0; i < size; i++) {
         a = udt_top_left(d);        
-        if (a.key > max) {                    // Сравниваем ключи
-            max = udt_top_left(d).key;        // Обновляем максимальный ключ
+        if (a > max) {                    // Сравниваем ключи
+            max = udt_top_left(d);        // Обновляем максимальный ключ
             t = udt_top_left(d);              // Сохраняем текущий элемент как максимальный
         }
         udt_pop_front(d);                     // Удаляем первый элемент
         udt_push_back(d, a);                  // Добавляем его в конец
     }
     udt *d1 = create_udt(NULL);
-    while (udt_top_left(d).key != t.key && udt_top_right(d).key != t.key) {
+    while (udt_top_left(d) != t && udt_top_right(d) != t) {
         a = udt_top_left(d);
         udt_push_front(d1, a);                // Добавляем элемент в начало вспомогательного дека
         udt_pop_front(d);                     // Удаляем первый элемент
@@ -29,7 +29,7 @@ data procedure(udt *d) {                      // Находит максимал
             udt_pop_back(d);                  // Удаляем последний элемент
         }
     }
-    if (udt_top_left(d).key == t.key) {
+    if (udt_top_left(d) == t) {
         udt_pop_front(d);                     // Удаляем максимальный элемент
     } else {
         udt_pop_back(d);                      // Удаляем максимальный элемент
@@ -44,7 +44,7 @@ data procedure(udt *d) {                      // Находит максимал
 udt* sort(udt *d) {                           // Использует procedure, чтобы находить максимальный элемент и добавлять его
     int size = udt_size(d);                   // в новый дек в порядке убывания. Он повторяет эту операцию до тех пор, пока 
     udt *d1 = create_udt(NULL);               // все элементы не будут перемещены в новый дек. На выходе он возвращает новый дек, 
-    data t;                                   // содержащий все элементы в порядке убывания.
+    int t;                                   // содержащий все элементы в порядке убывания.
     for (int i = 0; i < size; i++) {
         t = procedure(d);                     // Получаем максимальный элемент и удаляем его
         udt_push_front(d1, t);                // Добавляем максимальный элемент в начало нового дека
@@ -56,11 +56,13 @@ int main() {
     int c = 1, ans;
     udt *d = NULL;
     while (c) {
-        printf("1. Create deque\n2. Empty\n3. Size\n4. Push back\n5. Push front\n6. Top left\n7. Top right\n8.Pop back\n9. Pop front\n10.Print\n11. Sort\n12. Exit\n");
+        printf("\n1. Create deque\n2. Empty\n3. Size\n4. Push back\n5. Push front\n6. Top left\n7. Top right\n8. Pop back\n9. Pop front\n10. Print\n11. Sort\n12. Exit\n");
+        printf("Enter an action:\n");
         scanf("%d", &ans);
         switch (ans) {
             case 1: {
                 d = create_udt(d);                                                             // Создаем дек
+                printf("Deque created\n");
                 break;
             }
             case 2: {
@@ -83,11 +85,9 @@ int main() {
                 if (d == NULL) {
                     printf("Deque doesn't exist\n");
                 } else {
-                    data tb;
-                    printf("Print key\n");
-                    scanf("%d", &tb.key);
-                    printf("Print string\n");
-                    scanf("%s", tb.value);
+                    int tb;
+                    printf("Enter a value:\n");
+                    scanf("%d", &tb);
                     if (!udt_push_back(d, tb)) {                                              // Добавляем элемент в конец дека
                         printf("Deque is full\n");
                     }
@@ -98,11 +98,9 @@ int main() {
                 if (d == NULL) {
                     printf("Deque doesn't exist\n");
                 } else {
-                    data tf;
-                    printf("Print key\n");
-                    scanf("%d", &tf.key);
-                    printf("Print string\n");
-                    scanf("%s", tf.value);
+                    int tf;
+                    printf("Enter a value:\n");
+                    scanf("%d", &tf);
                     if (!udt_push_front(d, tf)) {                                             // Добавляем элемент в начало дека
                         printf("Deque is full\n");
                     }
@@ -116,8 +114,8 @@ int main() {
                     if (udt_empty(d)) {
                         printf("Deque is empty\n");
                     } else {
-                        data a = udt_top_left(d);                                           // Получаем первый элемент
-                        printf("Key\n%d\nValue\n%s\n", a.key, a.value);                 // Выводим ключ и значение элемента
+                        int a = udt_top_left(d);                                           // Получаем первый элемент
+                        printf("Value: %d\n", a);                 // Выводим ключ и значение элемента
                     }
                 }
                 break;
@@ -129,8 +127,8 @@ int main() {
                     if (udt_empty(d)) {
                         printf("Deque is empty\n");
                     } else {
-                        data a = udt_top_right(d);                                          // Получаем последний элемент
-                        printf("Key\n%d\nValue\n%s\n", a.key, a.value);                 // Выводим ключ и значение элемента
+                        int a = udt_top_right(d);                                          // Получаем последний элемент
+                        printf("Value: %d\n", a);                 // Выводим ключ и значение элемента
                     }
                 }
                 break;
@@ -160,6 +158,7 @@ int main() {
                     printf("Deque doesn't exist\n");
                 } else {
                     udt_print(d);                                                         // Выводим все элементы дека
+                    printf("\n");
                 }
                 break;
             }
