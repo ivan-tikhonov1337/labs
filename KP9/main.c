762d79a07b87176a4a831557534272b5c837be22
ivan-tikhonov1337 <127095794+ivan-tikhonov1337@users.noreply.github.com>
2023-05-28 13:02:22 +0300

    Update main.c

--- KP9/main.c
+++ KP9/main.c
@@ -3,64 +3,89 @@
 #include <string.h>
 #include "table.h"
 
-int main() {
-    struct node table[MAX_SIZE];
-    int size, i, choice;
-    char key[MAX_LENGTH];
-    printf("Введите количество элементов таблицы (от 12 до 14): ");
-    scanf("%d", &size);
-    if (size < 12 || size > 14) {
-        printf("Ошибка: неверное количество элементов таблицы\n");
-        return 1;
+
+int main(int argc, char *argv[]) {
+    List* l = Create_List();
+    FILE *in = fopen(argv[1], "r"); // открытие файла для чтения
+    if (!in) {
+        printf("I/O Error: can't open file.\n");
+        Free_List(l);
+        exit(1);
     }
-    printf("Введите элементы таблицы:\n");
-    for (i = 0; i < size; i++) {
-        printf("Ключ и значение элемена %d:\n", i + 1);
-        scanf("%s %s", table[i].key, table[i].data);
-        printf("%s %s %d\n",table[i].key, table[i].data, strlen(table[i].key));
-        if (strlen(table[i].key) > MAX_LENGTH) {
-            printf("Ошибка: длина ключа не может превышать %d символов\n", MAX_LENGTH);
-            return 1;
+    char *filename = argv[1];
+    Read_Table(l, in); // чтение таблицы из файла
+    fclose(in);
+    if (Is_List_Sorted(l, true)) { // проверка на порядок сортировки
+        l->sorted = 1;
+    } else if (Is_List_Sorted(l, false)) {
+        l->sorted = -1;
+    } else {
+        l->sorted = 0;
+    }
+
+    char what_do[STR_SIZE];
+    print_menu();
+    while (!feof(stdin)) {
+        fscanf(stdin, "%s", what_do);
+        if (feof(stdin)) {
+            break;
         }
-    }
-    printf("\n");
-    printf("Исходное состояние таблицы:\n");
-    print_table(table, size);
-    printf("\n");
-    while(1) {
-        printf("Выберите действие:\n");
-        printf("1. Отсортировать таблицу\n");
-        printf("2. Поиск по таблице\n");
-        printf("3. Вывести таблицу\n");
-        printf("4. Выйти из программы\n");
-        scanf("%d", &choice);
-        switch (choice) {
-            case 1:
-                printf("Сортировка методом пузырька:\n");
-                bubble_sort(table, size);
-                print_table(table, size);
-                break;
-            case 2:
-                printf("Поиск по таблице:\n");
-                printf("Введите ключ для поиска: ");
-                scanf("%s", key);
-                int result = binary_search(table, size, key);
-                if (result == -1) {
-                    printf("Запись с ключом %s не найдена\n", key);
+        if (!strcmp(what_do, "print")) {
+            Table_Print(l);
+        }else if (!strcmp(what_do, "add")) {
+            char key[7];
+            char data[STR_SIZE];
+            scanf("%s", key);
+            fgets(data, STR_SIZE, stdin);
+            Add_To_List(l, key, data);
+            l->sorted = 0;
+            printf("Запись добавлена.\n");
+        } else if (!strcmp(what_do, "remove")) {
+            Table_Pop(l);
+            printf("Последняя запись удалена.\n");
+        } else if (!strcmp(what_do, "exit")) {
+            break;
+        } else if (!strcmp(what_do, "overwriting")) {
+            FILE *out = fopen(filename, "w");
+            Write_List(out, l);
+            fclose(out);
+        } else if (!strcmp(what_do, "find")) {
+            char key[7];
+            scanf("%s", key);
+            if (l->sorted == 0) {
+                printf("Отсортируйте, пожалуйста, список!\n");
+            } else if (l->sorted == -1) {
+                Reverse_List(l);
+                char* result = Binary_Search(l, key);
+                if (result == NULL) {
+                    printf("Введённый ключ не найден.\n");
+                } else {
+                    printf("%s\n", result);
                 }
-                else {
-                    printf("Найденная запись: %s %s\n", table[result].key, table[result].data);
+            } else {
+                char* result = Binary_Search(l, key);
+                if (result == NULL) {
+                    printf("Введённый ключ не найден.\n");
+                } else {
+                    printf("%s\n", result);
                 }
-                break;
-            case 3:
-                print_table(table, size);
-                break;
-            case 4:
-                return 0;
-            default:
-                printf("Неверный ввод\n");
-                break;
+            }
+        } else if (!strcmp(what_do, "sort")) {
+            Bubble_Sort(l);
+            l->sorted = 1;
+            printf("Список отсортирован:)\n");
+        } else if (!strcmp(what_do, "issort")) {
+            if (l->sorted == 1) {
+                printf("Список отсортирован по возрастанию.\n");
+            } else if (l->sorted == -1) {
+                printf("Список отсортирован по убыванию.\n");
+            } else {
+                printf("Список не отсортирован.\n");
+            }
+        } else {
+            printf("Введена неправильная команда.\n");
         }
+        print_menu();
     }
-    return 0;
+    Free_List(l); // освобождение памяти, занятой списком
 }
