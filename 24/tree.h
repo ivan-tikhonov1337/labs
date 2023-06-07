dd595d61a69b0420cd8be68adfe77d1fe17bc77e
ivan-tikhonov1337 <127095794+ivan-tikhonov1337@users.noreply.github.com>
2023-06-07 22:23:02 +0300

    Create tree.h

---
+++ 24/tree.h
@@ -0,0 +1,26 @@
+#ifndef tree_h
+#define tree_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef struct unit {
+       char data [50];
+       struct unit *left;
+       struct unit *right;
+}unit;
+
+int define_priority (char a);
+
+unit * make_tree(char expr[], int first, int last);
+
+void print_tree(unit *u, int l);
+
+void print_expression(unit *u);
+
+unit * spec_func(unit *u);
+
+
+#endif
